f512a3682d317cf270912b59e6868b91737c1515
Tung Bui <tung.bquang@gmail.com>
2023-06-24 13:25:26 +0700

    add nolint.4

--- internal/osutil/file_test.go
+++ internal/osutil/file_test.go
@@ -226,7 +226,7 @@ func TestCopyToDir(t *testing.T) {
 		if err := os.Chmod(destTempDir, 0000); err != nil {
 			t.Fatal(err)
 		}
-		defer os.Chmod(destTempDir, 0700)
+		defer os.Chmod(destTempDir, 0700) //nolint:errcheck
 		if _, err := CopyToDir(filename, destTempDir); err == nil {
 			t.Fatal("should have error")
 		}
