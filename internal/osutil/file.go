c044a3b621f873581edcdbb9907744bde0c383c1
Tung Bui <tung.bquang@gmail.com>
2023-07-12 21:45:16 +0700

    fix linter

--- internal/osutil/file.go
+++ internal/osutil/file.go
@@ -81,3 +81,85 @@ func IsRegularFile(path string) (bool, error) {
 
 	return fileStat.Mode().IsRegular(), nil
 }
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// WriteFile writes to a path with all parent directories created.
+func WriteFile(path string, data []byte) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0o700); err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0o600)
+}
+
+// WriteFileWithPermission writes to a path with all parent directories created.
+func WriteFileWithPermission(path string, data []byte, perm fs.FileMode, overwrite bool) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0o700); err != nil {
+		return err
+	}
+	flag := os.O_WRONLY | os.O_CREATE
+	if overwrite {
+		flag |= os.O_TRUNC
+	} else {
+		flag |= os.O_EXCL
+	}
+	file, err := os.OpenFile(path, flag, perm)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(data)
+	if err != nil {
+		file.Close()
+		return err
+	}
+	return file.Close()
+}
+
+// CopyToDir copies the src file to dst. Existing file will be overwritten.
+func CopyToDir(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	if err := os.MkdirAll(dst, 0o700); err != nil {
+		return 0, err
+	}
+	certFile := filepath.Join(dst, filepath.Base(src))
+	destination, err := os.Create(certFile)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	err = destination.Chmod(0o600)
+	if err != nil {
+		return 0, err
+	}
+	return io.Copy(destination, source)
+}
+
+// IsRegularFile checks if path is a regular file
+func IsRegularFile(path string) (bool, error) {
+	fileStat, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+
+	return fileStat.Mode().IsRegular(), nil
+}
